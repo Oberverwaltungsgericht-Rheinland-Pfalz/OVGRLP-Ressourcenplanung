db1aac6baeccd6e17c7732756f319679c4c4360f
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2019-12-30 09:49:31 +0100

    VueApp: Zugriff auf WebApi nun über proxy mit Authentifizierung  möglich

--- Raumplanung.VueWebApp/vue.config.js
+++ Raumplanung.VueWebApp/vue.config.js
@@ -1,7 +1,31 @@
 // vue.config.js
+const Agent = require('agentkeepalive');
+
 module.exports = {
   devServer: {
     port: 5002,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:52575',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': 'api'
+        },
+        logLevel: 'debug',
+        agent: new Agent({
+          maxSockets: 100,
+          keepAlive: true,
+          maxFreeSockets: 10,
+          keepAliveMsecs: 1000,
+          timeout: 60000,
+          freeSocketTimeout: 30000
+        }),
+        onProxyRes: proxyRes => {
+          let key = 'www-authenticate';
+          proxyRes.headers[key] = proxyRes.headers[key] && proxyRes.headers[key].split(',');
+        }
+      }
+    }
   },
 /*  devServer: {
     proxy: {
