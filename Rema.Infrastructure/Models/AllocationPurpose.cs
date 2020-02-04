b64dcd1cd6db7f06ad58b6a7d964755e99f0abb2
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2020-02-04 09:57:39 +0100

    Api: AllocationPurpose als veraltet markiert

--- Rema.Infrastructure/Models/AllocationPurpose.cs
+++ Rema.Infrastructure/Models/AllocationPurpose.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Rema.Infrastructure.Models
 {
+  [Obsolete]
   public class AllocationPurpose
   {
     public AllocationPurpose()
