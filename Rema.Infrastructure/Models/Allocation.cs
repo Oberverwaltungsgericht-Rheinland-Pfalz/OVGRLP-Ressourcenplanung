86a75c314e72b56aa80522190b8552d0c7a4e9ff
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2020-02-04 09:58:27 +0100

    Model: Titel, Contact, Gadgets in Allocation hinzugefügt und Purpose entfernt

--- Rema.Infrastructure/Models/Allocation.cs
+++ Rema.Infrastructure/Models/Allocation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
@@ -15,28 +16,42 @@ namespace Rema.Infrastructure.Models
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
 
+    [Required]
+    public string Title { get; set; }
+
+    public string ContactName { get; set; }
+
+    public string ContactPhone { get; set; }
+    
+    [MaxLength(3000)]
+    public string Notes { get; set; }
+
     public DateTime From { get; set; }
+
     public DateTime To { get; set; }
+
     public Boolean IsAllDay { get; set; }
 
     public MeetingStatus Status { get; set; }
 
     public virtual Ressource Ressource { get; set; }
-
-    public virtual AllocationPurpose Purpose { get; set; }
-
+    
     [Required]
     public virtual User CreatedBy { get; set; }
 
     public DateTime CreatedAt { get; set; }
 
     public DateTime LastModified { get; set; }
+
     public User LastModifiedBy { get; set; }
 
     public virtual User ApprovedBy { get; set; }
+
     public DateTime ApprovedAt { get; set; }
 
     public virtual User ReferencePerson { get; set; }
+
+    public virtual ICollection<GadgetPurpose> Gadgets { get; set; } = new List<GadgetPurpose>();
   }
 
   public enum MeetingStatus
