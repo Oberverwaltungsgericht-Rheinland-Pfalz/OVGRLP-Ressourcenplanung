92880c0b2da8a7d53ec64994434872285ff46b97
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2020-02-07 07:29:13 +0100

    Api: AllocationViewModel geändert und enthaltene Klassen in eigene Dateien verschoben

---
+++ Rema.WebApi/MappingProfiles/AllocationMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Rema.Infrastructure.Models;
+using Rema.WebApi.ViewModels;
+
+namespace Rema.WebApi.MappingProfiles
+{
+  public class AllocationMappingProfile : Profile
+  {
+    public AllocationMappingProfile()
+    {
+      CreateMap<Allocation, AllocationViewModel>()
+        .ForMember(dest => dest.Ressource_id, opt => opt.MapFrom(src => src.Ressource.Id));
+
+      CreateMap<AllocationViewModel, Allocation>();
+    }
+  }
+}
---
+++ Rema.WebApi/ViewModels/AllocationRequestEdition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rema.WebApi.ViewModels
+{
+  public class AllocationRequestEdition
+  {
+    public long Id { get; set; }
+    public int status { get; set; }
+    public DateTime? From { get; set; }
+    public DateTime? To { get; set; }
+  }
+}
