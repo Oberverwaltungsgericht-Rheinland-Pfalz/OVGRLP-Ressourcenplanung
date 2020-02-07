d596bc03344df9517e5a4133b1bfd3d1a31bddf4
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2020-02-07 07:31:10 +0100

    Api: enthaltene Klassen aus UserViewModel.cs in eigene Dateien verschoben

--- Rema.WebApi/ViewModels/UserViewModel.cs
+++ Rema.WebApi/ViewModels/UserViewModel.cs
@@ -11,39 +11,4 @@ namespace Rema.WebApi.ViewModels
     public List<Role> Roles { get; set; } = new List<Role>();
     public IList<long> SupportGroupIds { get; set; }
   }
-
-  public class UserVMProfile : Profile
-  {
-    public UserVMProfile()
-    {
-      CreateMap<User, UserViewModel>()
-          .ForMember(dest => dest.SupportGroupIds, opt => opt.MapFrom(src => src.SupplierGroups.Select(e => e.Id)));
-
-      CreateMap<UserViewModel, User>();
-      CreateMap<User, ContactUser>()
-          .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Name));
-    }
-  }
-
-  public class Role
-  {
-    [JsonIgnore]
-    public string AdDescription { get; set; }
-
-    public string Name { get; set; }
-    public int Level { get; set; }
-
-    public bool HasRole(Role role)
-    {
-      return role.Level >= Level;
-    }
-  }
-
-  public class ContactUser
-  {
-    public long Id { get; set; }
-    public string Title { get; set; }
-    public string Email { get; set; }
-    public string Organisation { get; set; }
-  }
 }
