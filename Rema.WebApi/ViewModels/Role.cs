4ee19df44b22b1a8f589fc005f1661d3c2a35a46
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2020-02-07 07:31:10 +0100

    Api: enthaltene Klassen aus UserViewModel.cs in eigene Dateien verschoben

---
+++ Rema.WebApi/ViewModels/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Rema.WebApi.ViewModels
+{
+  public class Role
+  {
+    [JsonIgnore]
+    public string AdDescription { get; set; }
+
+    public string Name { get; set; }
+
+    public int Level { get; set; }
+
+    public bool HasRole(Role role)
+    {
+      return role.Level >= Level;
+    }
+  }
+}
