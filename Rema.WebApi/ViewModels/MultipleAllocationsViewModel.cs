a2e0ac0f7c1500640a77071e507033884002d361
Reiner Bamberger <Reiner.Bamberger@ovg.jm.rlp.de>
2020-02-21 16:13:59 +0100

    vue app: Serientermin Zeitfindung und Modell zur Übermittlung, Related To #57

---
+++ Rema.WebApi/ViewModels/MultipleAllocationsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rema.WebApi.ViewModels
+{
+  public class MultipleAllocationsViewModel : AllocationViewModel
+  {
+    public List<string> Dates { get; set; }
+  }
+}
