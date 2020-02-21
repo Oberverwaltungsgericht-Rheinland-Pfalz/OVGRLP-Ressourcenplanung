fc239021eb0c036bc4b288fdbb8ee8e49c604321
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2020-02-21 07:13:15 +0100

    DebugController mit "CurrentUserMembers" erstellt

---
+++ Rema.WebApi/Controllers/DebugController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Rema.DbAccess;
+using Rema.Infrastructure.Models;
+using Rema.WebApi.Filter;
+using Rema.WebApi.ViewModels;
+using Serilog;
+
+namespace Rema.WebApi.Controllers
+{
+  [Produces("application/json")]
+  [Route("[controller]")]
+  [ApiController]
+  public class DebugController : BaseController
+  {
+    public DebugController(RpDbContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    [Route("CurrentUserMembers")]
+    [HttpGet]
+    public IEnumerable<string> GetCurrentUserMembers()
+    {
+      var securityMembers = new List<string>();
+      var user = this.HttpContext.User;
+      var identity = (WindowsIdentity)user.Identity;
+
+      securityMembers.Add(identity.Name);
+      foreach (var group in identity.Groups)
+      {
+        string groupTitle = group.Translate(typeof(NTAccount)).ToString();
+        securityMembers.Add(groupTitle);
+      }
+
+      return securityMembers;
+    }
+  }
+}
