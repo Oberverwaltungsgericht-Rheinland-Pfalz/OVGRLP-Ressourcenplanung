1af6faab6449072bc0f1b57dfee855d0e01c1823
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2020-02-25 08:56:31 +0100

    VueApp: DateTimePicker verbessert (Uhrzeit geht bei ganzer Tag nicht verloren)

--- rema-app-vue/src/components/DateTimePicker.vue
+++ rema-app-vue/src/components/DateTimePicker.vue
@@ -15,19 +15,18 @@
               :placeholder="placeholder"
               :label="label"
               readonly
-              clearable
-              @click:clear="date = null"
               v-on="on"
             ></v-text-field>
           </template>
           <v-date-picker
-            v-model="date"
-            scrollable
-            @change="modal = false" />
+            :value="date"
+            @change="modal=false"
+            @input="dateChanged"
+            scrollable />
         </v-dialog>
       </v-col>
       <v-col cols="3" v-if="withTime">
-        <v-text-field v-model="time" type="time"></v-text-field>
+        <v-text-field :value="time" @input="timeChanged" type="time"></v-text-field>
       </v-col>
     </v-row>
     <v-row>
@@ -55,52 +54,53 @@ export default class DateTimePicker extends Vue {
   @Prop({ default: 'L' }) dateFormat!: string;
   @Prop({ default: '' }) value!: string;
 
-  private date: string = ''
-  private time: string = this.defaultTime
+  private internalValue = ''
+  private lastTime: string = ''
 
   modal: Boolean = false;
 
-  @Watch('withTime')
-  public withTimeChange (val: Boolean) {
-    this.$emit('input', this.getValue())
+  get date () {
+    if (this.internalValue.indexOf('T') > 0) {
+      return this.internalValue.split('T')[0]
+    } else {
+      return ''
+    }
+  }
+  dateChanged (val: string) {
+    this.updateValue(val, this.time)
+  }
+
+  get time () {
+    if (this.internalValue.indexOf('T') > 0) {
+      return this.internalValue.split('T')[1]
+    } else {
+      return this.defaultTime
+    }
+  }
+  timeChanged (val: string) {
+    this.updateValue(this.date, val)
+  }
+
+  updateValue (date: string, time: string) {
+    this.internalValue = `${date}T${time}`
+
+    const newValue = `${date}T${this.withTime ? time : this.defaultTimeFullDay}`
+    this.$emit('input', newValue)
   }
 
   @Watch('value')
-  public valueChange (val: string) {
-    let datePart = ''
-    let timePart = this.defaultTime
+  public valueChanged (val: string) {
+    this.internalValue = val
+  }
 
-    if (val && val.length > 0 && val.indexOf('T')) {
-      datePart = val.split('T')[0]
-      timePart = val.split('T')[1]
+  @Watch('withTime')
+  public withTimeChange (val: Boolean) {
+    if (val) {
+      this.updateValue(this.date, this.lastTime)
+    } else {
+      this.lastTime = this.time
+      this.updateValue(this.date, this.defaultTimeFullDay)
     }
-
-    this.date = datePart
-    this.time = timePart
-  }
-
-  @Watch('date')
-  public dateChange (val: string) {
-    this.$emit('input', this.getValue())
-  }
-
-  @Watch('time')
-  public timeChange (val: string) {
-    this.$emit('input', this.getValue())
-  }
-
-  private getValue () : string {
-    let newValue = ''
-
-    if (this.date && this.date.length > 0) {
-      if (!this.withTime) {
-        newValue = this.date + 'T' + this.defaultTimeFullDay
-      } else if (this.time && this.time.length > 0) {
-        newValue = this.date + 'T' + this.time
-      }
-    }
-
-    return newValue
   }
 
   get formatedDate (): string {
