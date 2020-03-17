e3f738888cb8980741b87375d1637bdcc104e980
Reiner Bamberger <Reiner.Bamberger@ovg.jm.rlp.de>
2020-03-17 13:32:05 +0100

    WebApi: Unterstützergruppen können Nachrichten je Termin erhalten, welche als JSON gespeichert werden

---
+++ Rema.Infrastructure/Models/SupplierHint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rema.Infrastructure.Models
+{
+  public class SupplierHint
+  {
+    public SupplierGroup Group { get; set; }
+    public string Message { get; set; }
+  }
+}
