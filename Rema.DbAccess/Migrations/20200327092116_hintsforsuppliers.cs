0ba4fb2a9e28c5bf743e7cc5ca40e77a1e43f227
Reiner Bamberger <Reiner.Bamberger@ovg.jm.rlp.de>
2020-03-27 11:42:26 +0100

    DB-Update für Hinweise an Unterstützergruppen, related #71

---
+++ Rema.DbAccess/Migrations/20200327092116_hintsforsuppliers.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rema.DbAccess.Migrations
+{
+    public partial class hintsforsuppliers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SupplierGroups_Users_UserId",
+                table: "SupplierGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SupplierGroups_UserId",
+                table: "SupplierGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "SupplierGroups");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SerializedHints",
+                table: "Allocations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SerializedHints",
+                table: "Allocations");
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "SupplierGroups",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupplierGroups_UserId",
+                table: "SupplierGroups",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SupplierGroups_Users_UserId",
+                table: "SupplierGroups",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ Rema.DbAccess/Migrations/Skripte/0-9-3-20135_Update.sql
@@ -0,0 +1,27 @@
+﻿ALTER TABLE [SupplierGroups] DROP CONSTRAINT [FK_SupplierGroups_Users_UserId];
+
+GO
+
+DROP INDEX [IX_SupplierGroups_UserId] ON [SupplierGroups];
+
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[SupplierGroups]') AND [c].[name] = N'UserId');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [SupplierGroups] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [SupplierGroups] DROP COLUMN [UserId];
+
+GO
+
+ALTER TABLE [Allocations] ADD [SerializedHints] nvarchar(max) NULL;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200327092116_hintsforsuppliers', N'3.1.0');
+
+GO
+
