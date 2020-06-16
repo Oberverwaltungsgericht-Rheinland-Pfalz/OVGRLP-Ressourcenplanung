86c1e123f19599b1472c9d30c0d31c4e5b9cdc56
Reiner Bamberger <Reiner.Bamberger@ovg.jm.rlp.de>
2020-06-16 14:46:09 +0200

    vueapp: Termine ausdrucken 2/2

---
+++ Rema.Infrastructure/Email/Templates/PrintTemplate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Rema.Infrastructure.Models;
+
+namespace Rema.Infrastructure.Email.Templates
+{
+  public class PrintTemplate : EmailTemplate
+  {
+    public PrintTemplate (Allocation allocation) : base(allocation)
+    {
+    }
+    private string _newStatus { get; set; }
+
+    public override string Subject => "";
+
+    public override string ToString()
+    {
+      return $@"Termineintrag in der Ressourcenplanung.
+Von: {LastModifier}
+
+Raum:
+{RessourceName} 
+
+Titel:
+{Title}
+
+Reservierung:
+{ReserveTime}
+
+Ansprechpartner:
+{ReferencePerson}
+
+Telefonnummer:
+{ContactPhone}
+
+Hilfsmittel:{GroupsGadgets}
+
+Notizen:
+{Notes}
+{HintsForSuppliersText}
+";
+    }
+  }
+}
