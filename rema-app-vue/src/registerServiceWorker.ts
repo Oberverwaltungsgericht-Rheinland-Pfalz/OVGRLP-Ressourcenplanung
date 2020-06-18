5c67a998e679d4df204dfb2252181a32227566dd
Reiner Bamberger <Reiner.Bamberger@ovg.jm.rlp.de>
2020-06-18 12:40:17 +0200

    vueapp: fix Termindarstellung

--- rema-app-vue/src/registerServiceWorker.ts
+++ rema-app-vue/src/registerServiceWorker.ts
@@ -1,5 +1,5 @@
 import { register } from 'register-service-worker'
-
+/*
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
@@ -24,3 +24,4 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+*/
