dae1354dda6b63c4562b3c93fab9e85b3f521c80
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2021-01-25 10:10:56 +0100

    WebApi: Email-Remainder modell Änderung

---
+++ Rema.Infrastructure/Email/Templates/RemindTemplate.cs
@@ -0,0 +1,50 @@
+﻿using Rema.Infrastructure.Models;
+
+namespace Rema.Infrastructure.Email.Templates
+{
+  public class RemindTemplate : EmailTemplate
+  {
+    public RemindTemplate(Allocation allocation) : base(allocation)
+    {
+    }
+    private string _newStatus { get; set; }
+
+    public override string Subject => $"Terminerinnerung {this._allocation.Title} am {this._allocation.From}";
+
+    public override string ToString()
+    {
+      return $@"Möchten Sie Ihren Termin #{this._allocation.Id} am {this._allocation.From} in der Ressourcenplanung
+wie geplant wahrnehmen?
+Wenn Ja müssen sie nichts tun, ansonsten löschen Sie diesen falls möglich oder 
+setzen sie sich bitte mit Ihrer zuständigen Verwaltunsstelle in Verbindung.
+
+Ansprechpartner:
+{ReferencePerson}
+
+Raum: {RessourceName} 
+
+Titel:
+{Title}
+
+Status:
+{Status}
+
+Reservierung:
+{ReserveTime}
+
+Telefonnummer:
+{ContactPhone}
+
+Hilfsmittel:{GroupsGadgets}
+
+Notizen:
+{Notes}
+{HintsForSuppliersText}
+
+Erstellt durch: {this._allocation.CreatedBy?.Name}
+Zuletzt geändert durch: {LastModifier}
+Zuletzt geändert am: {this._allocation.LastModified.ToString("dddd, dd MMMM yyyy")}
+";
+    }
+  }
+}
---
+++ Rema.ServiceLayer/Jobs/RemindJob.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Quartz;
+using Rema.DbAccess;
+using Rema.Infrastructure.Email;
+using Rema.Infrastructure.Email.Templates;
+using Rema.Infrastructure.Models;
+using Serilog;
+
+namespace Rema.ServiceLayer.Jobs
+{
+  public interface IRemindJob : IJob { }
+
+  public class RemindJob : IRemindJob
+  {
+    protected readonly RpDbContext _context;
+//    protected readonly IMapper _mapper;
+    protected readonly IEmailTrigger _emailTrigger;
+
+    public RemindJob(RpDbContext context, IEmailTrigger emailTrigger)
+    {
+      this._context = context;
+      this._emailTrigger = emailTrigger;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+      var timeRange = DateTime.Now.AddDays(10);
+      IList<Allocation> unreminded = new List<Allocation>();
+      try
+      {
+       unreminded = await _context.Allocations
+      .Include(o => o.Ressources)
+      .Include(r => r.ReferencePerson)
+      .Include(r => r.LastModifiedBy)
+      .Include(r => r.CreatedBy)
+      .Include(g => g.AllocationGadgets).ThenInclude(ag => ag.Gadget).ThenInclude(g => g.SuppliedBy)
+      .Where(a => a
+        .ReferencePerson != null && 
+        a.From > DateTime.Now && 
+        a.From < timeRange
+        && !a.Reminded // was reminded boolean
+        )
+      .ToListAsync();
+      }
+      catch (Exception ex)
+      {
+        Log.Error(ex, "error while getting allocations");
+        return;
+      }
+
+      foreach(var remindAllocation in unreminded)
+      {
+        var template = new RemindTemplate(remindAllocation);
+        this._emailTrigger.SendEmail(template, null, new List<string>(){ remindAllocation.ReferencePerson.Email });
+
+        remindAllocation.Reminded = true;
+      }
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      } catch (Exception ex)
+      {
+        Log.Error(ex, "error while saving reminded allocations");
+      }
+    }
+  }
+}
