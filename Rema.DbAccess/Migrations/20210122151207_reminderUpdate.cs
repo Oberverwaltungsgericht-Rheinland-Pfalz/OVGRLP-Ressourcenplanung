fb0d1a1366dd6fe5753cbba84015166af71083e9
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2021-01-25 13:27:06 +0100

    Datenbankänderung für Reminder

---
+++ Rema.DbAccess/Migrations/20210122151207_reminderUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rema.DbAccess.Migrations
+{
+    public partial class reminderUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Reminded",
+                table: "Allocations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reminded",
+                table: "Allocations");
+        }
+    }
+}
