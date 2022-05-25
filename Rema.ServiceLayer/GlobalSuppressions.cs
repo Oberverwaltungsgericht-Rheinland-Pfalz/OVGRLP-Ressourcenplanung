ed91164f8d2b5ce7aab057428af7d824971f6705
reinerBa <4329883+reinerBa@users.noreply.github.com>
2022-05-25 09:20:06 +0200

    Unterdücke CA1416 "nur Windows"-Warnung

---
+++ Rema.ServiceLayer/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Rema.ServiceLayer.Services.AdService.SearchAdUsers``1(System.String)~System.Collections.Generic.List{``0}")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Rema.ServiceLayer.Services.AdService.GetMemberListByUserName(System.String,System.String)~System.Collections.Generic.List{System.String}")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Rema.ServiceLayer.Services.AdService.FindDomainByName(System.String)~System.DirectoryServices.ActiveDirectory.Domain")]
